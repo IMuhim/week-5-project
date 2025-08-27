74216a4d2e98a6dbe070be216dea82a948882e75
ReeceKirkham24 <reececallumkirkham24@gmail.com>
2025-08-27 16:42:32 +0100

    working getAllBySubjectId and getSubjectById unit tests

--- server/__tests__/unit/models/Game.test.js
+++ server/__tests__/unit/models/Game.test.js
@@ -1,3 +1,4 @@
+const e = require("express");
 const db = require("/server/db/connect.js");
 const{ Questions } = require("/server/models/Game.js");
 const{ Answers } = require("/server/models/Game.js");
@@ -31,6 +32,22 @@ describe("Questions", () => {
             // ACT
             await expect(Questions.getAllBySubjectId(9999)).rejects.toThrow("Unable to locate questions with that subject id")
             // ASSERT
+        })
+    })
+
+    describe("getSubjectById", () => {
+
+        it("Gives subject name from subjects with matching subject_id", async () => {
+            const mockSubject = [{ subject_id: 1, subject_name: "Mock subject" }];
+            jest.spyOn(db, "query").mockResolvedValueOnce({ rows: mockSubject });
+            const result = await Questions.getSubjectById(1);
+            expect(result).toBe("Mock subject");
+            expect(db.query).toHaveBeenCalledWith('SELECT subject_name FROM subjects WHERE subject_id = $1', [1]);
+        })
+
+        it("Should throw Error when a subject with this id is not found", async () => {
+            jest.spyOn(db, "query").mockResolvedValueOnce({ rows: [] });
+            await expect(Questions.getSubjectById(9999)).rejects.toThrow("Unable to to locate subject with that id");
         })
     })
 })
